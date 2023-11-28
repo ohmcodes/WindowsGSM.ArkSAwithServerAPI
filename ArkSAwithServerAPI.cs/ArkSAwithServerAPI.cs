d3b9f07e5e6b021330f9f729d2017316f2127ce4
Ohmcodes <ohmcodes@gmail.com>
2023-11-28 01:29:02 -0800

    final version 2

--- ArkSAwithServerAPI.cs/ArkSAwithServerAPI.cs
+++ ArkSAwithServerAPI.cs/ArkSAwithServerAPI.cs
@@ -9,27 +9,43 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using Newtonsoft.Json;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Threading;
+using Newtonsoft.Json.Linq;
 
+using System.Collections.Generic;
+using System.IO.Compression;
 
 namespace WindowsGSM.Plugins
 {
-    public class ArkSurvivalAscended : SteamCMDAgent
+    public class ArkSAwithServerAPI : SteamCMDAgent
     {
         // - Plugin Details
         public Plugin Plugin = new Plugin
         {
             name = "WindowsGSM.ArkSAwithServerAPI", // WindowsGSM.XXXX
             author = "ohmcodes™",
-            description = "WindowsGSM plugin for supporting Ark Survival Ascended Dedicated Server with ServerAPI from GameServerHub",
-            version = "1.0.0",
+            description = "WindowsGSM plugin for supporting ARK:SA™ Dedicated Server with ServerAPI from GameServerHub",
+            version = "2.0.0",
             url = "https://github.com/ohmcodes/WindowsGSM.ArkSAwithServerAPI", // Github repository link (Best practice)
             color = "#008B8B" // Color Hex
         };
 
         // - Standard Constructor and properties
-        public ArkSurvivalAscended(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        public ArkSAwithServerAPI(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
         private readonly ServerConfig _serverData;
         public string Error, Notice;
+
+        private readonly string downloadUrl = "https://aka.ms/vs/17/release/vc_redist.x64.exe";
+        private readonly string reqFileName = "VC_redist.x64.exe";
+        public readonly string registryPath = @"SOFTWARE\Microsoft\VisualStudio\14.0_Config\VC\Runtimes\X64";
+        private readonly Version requiredVersion = new Version("14.38.33.130");
+
+        private readonly string serverAPIDownloadLink = "https://github.com/ServersHub/ServerAPI/releases/download/1.0/AsaApi.v0.1.zip";
+        private readonly string serverAPIFileName = "AsaApi.v0.1.zip";
+
+        private List<string> filesToDelete;
 
         // - Settings properties for SteamCMD installer
         public override bool loginAnonymous => true; 
@@ -37,7 +53,7 @@ namespace WindowsGSM.Plugins
 
         // - Game server Fixed variables
         public string StartPath = @"ShooterGame\Binaries\Win64\ArkAscendedServer.exe"; // Game server start path
-        public string FullName = "Ark Survival Ascended Dedicated Server with ServerAPI"; // Game server FullName
+        public string FullName = "ARK:SA™ Dedicated Server with ServerAPI"; // Game server FullName
         public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
         public int PortIncrements = 2; // This tells WindowsGSM how many ports should skip after installation
         public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
@@ -51,17 +67,25 @@ namespace WindowsGSM.Plugins
         public string QueryPort = "27015"; // Default query port
         public string Additional = "?RCONEnabled=True?RCONPort=31001?ServerAutoForceRespawnWildDinosInterval=86400?AllowCrateSpawnsOnTopOfStructures=True -UseBattlEye -server -crossplay -nosteamclient -servergamelog -log -lowmem -nosound -ServerRCONOutputTribeLogs -culture=en -servergamelogincludetribelogs -forcerespawndinos"; // Additional server start parameter
 
-
         // - Create a default cfg for the game server after installation
-        public async void CreateServerCFG()
+        public void CreateServerCFG()
         {
-            //No config file seems
+            if (!IsRedistributableInstalled(requiredVersion))
+            {
+                Notice = "Downloading VC_Distribution";
+                DownloadVCPackage();
+            }
+
+            DownloadServerAPI();
+            InstallServerAPI();
         }
 
         // - Start server function, return its Process to WindowsGSM
         public async Task<Process> Start()
         {
-            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            await Task.Delay(1);
+            string overrideStartPath = @"ShooterGame\Binaries\Win64\AsaApiLoader.exe";
+            string shipExePath = ServerPath.GetServersServerFiles(_serverData.ServerID, overrideStartPath);
             if (!File.Exists(shipExePath))
             {
                 Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
@@ -84,11 +108,9 @@ namespace WindowsGSM.Plugins
 
             param.Append(!string.IsNullOrWhiteSpace(_serverData.ServerMaxPlayer)? $"?MaxPlayers={_serverData.ServerMaxPlayer}" : $"?MaxPlayers=70");
 
-            param.Append(!string.IsNullOrWhiteSpace(_serverData.ServerMaxPlayer)? $" -WinLiveMaxPlayers={_serverData.ServerMaxPlayer}" : $"?MaxPlayers=70");
-
             if (!string.IsNullOrWhiteSpace(_serverData.ServerParam))
             {
-                if(_serverData.ServerParam.StartsWith("?"))
+                if (_serverData.ServerParam.StartsWith("?"))
                 {
                     param.Append($"{_serverData.ServerParam}");
                 }
@@ -98,14 +120,17 @@ namespace WindowsGSM.Plugins
                 }
             }
 
+            param.Append(!string.IsNullOrWhiteSpace(_serverData.ServerMaxPlayer)? $" -WinLiveMaxPlayers={_serverData.ServerMaxPlayer}" : $"?MaxPlayers=70");
+
+
             Process p = new Process
             {
                 StartInfo =
                 {
-                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID, @"ShooterGame\Binaries\Win64\"),
                     FileName = shipExePath,
-                    Arguments = param,
-                    WindowStyle = ProcessWindowStyle.Minimized,
+                    Arguments = param.ToString(),
+                    WindowStyle = ProcessWindowStyle.Normal,
                     UseShellExecute = false
                 },
                 EnableRaisingEvents = true
@@ -142,23 +167,15 @@ namespace WindowsGSM.Plugins
             }
         }
 
-
         // - Stop server function
         public async Task Stop(Process p)
         {
-            await Task.Run(async () =>
+            await Task.Run(() =>
             {
-                if (p.StartInfo.CreateNoWindow)
-                {
-                    p.CloseMainWindow();
-                }
-                else
-                {
-                    ServerConsole.SetMainWindow(p.MainWindowHandle);
-                    ServerConsole.SendWaitToMainWindow("DoExit");
-                    ServerConsole.SendWaitToMainWindow("{ENTER}");
-                    await Task.Delay(6000);
-                }
+                ServerConsole.SetMainWindow(p.MainWindowHandle);
+                ServerConsole.SendWaitToMainWindow("^c");
+                Thread.Sleep(1000);
+                ServerConsole.SendWaitToMainWindow("^c");
             });
         }
 
@@ -167,7 +184,6 @@ namespace WindowsGSM.Plugins
             var steamCMD = new Installer.SteamCMD();
             Process p = await steamCMD.Install(_serverData.ServerID, string.Empty, AppId, true, loginAnonymous);
             Error = steamCMD.Error;
-
             return p;
         }
 
@@ -175,12 +191,19 @@ namespace WindowsGSM.Plugins
         {
             var (p, error) = await Installer.SteamCMD.UpdateEx(_serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
             Error = error;
+
+            DownloadServerAPI();
+            await Task.Delay(5000);
+            CleanServerAPI();
+            await Task.Delay(5000);
+            InstallServerAPI();
+
             return p;
         }
 
         public bool IsInstallValid()
         {
-            return File.Exists(Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath));
+            return File.Exists(ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath));
         }
 
         public bool IsImportValid(string path)
@@ -202,5 +225,218 @@ namespace WindowsGSM.Plugins
             return await steamCMD.GetRemoteBuild(AppId);
         }
 
+        private bool IsRedistributableInstalled(Version requiredVersion)
+        {
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio\14.0\VC\Runtimes\x64"))
+            {
+                if (key != null)
+                {
+                    foreach (var subKeyName in key.GetSubKeyNames())
+                    {
+                        Trace.WriteLine($"Key {subKeyName}");
+                        using (RegistryKey subKey = key.OpenSubKey(subKeyName))
+                        {
+                            if (subKey != null)
+                            {
+                                Version installedVersion = new Version(subKey.GetValue("Version")?.ToString() ?? "0.0.0.0");
+
+                                if (installedVersion > requiredVersion)
+                                {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private async void DownloadVCPackage()
+        {
+            if (File.Exists(reqFileName))
+            {
+                // Delete zip file
+                await FileManagement.DeleteAsync(reqFileName);
+
+            }
+            WebClient webClient = new WebClient();
+            webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+            webClient.DownloadFileCompleted += WebClient_DownloadFileCompletedVCPackage;
+
+            try
+            {
+                webClient.DownloadFileAsync(new Uri(downloadUrl), ServerPath.GetServersServerFiles(_serverData.ServerID, reqFileName));
+            }
+            catch (Exception ex)
+            {
+                Error = $"Error downloading installer: {ex.Message}";
+            }
+        }
+
+        private void InstallVCPackage()
+        {
+            if (File.Exists(reqFileName))
+            {
+                try
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo(ServerPath.GetServersServerFiles(_serverData.ServerID, reqFileName));
+                    Process.Start(startInfo);
+
+                }
+                catch (Exception ex)
+                {
+                    Error = $"Error installing redistributable: {ex.Message}";
+                }
+            }
+            else
+            {
+                Error = "Installer not found. Please download it first.";
+            }
+        }
+
+        private async void DownloadServerAPI()
+        {
+            string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3";
+
+            if (File.Exists(serverAPIFileName))
+            {
+                File.Delete(serverAPIFileName);
+            }
+
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add(HttpRequestHeader.UserAgent, userAgent);
+            try
+            {
+                // Download the latest release information from the GitHub API
+                string apiUrl = $"https://api.github.com/repos/ServersHub/ServerAPI/releases/latest";
+                string responseContent = webClient.DownloadString(apiUrl);
+                JObject releaseInfo = JObject.Parse(responseContent);
+
+                // Get the download URL of the first asset (assuming there is at least one asset)
+                JToken asset = releaseInfo["assets"]?.FirstOrDefault();
+                string downloadUrl = (asset["browser_download_url"].ToString()).Trim();
+                webClient.DownloadFileAsync(new Uri(downloadUrl), ServerPath.GetServersServerFiles(_serverData.ServerID, serverAPIFileName));
+                Thread.Sleep(5000);
+            }
+            catch (WebException ex)
+            {
+                // Handle exceptions
+                Error = $"Error: {ex.Message}";
+            }
+
+            return;
+        }
+        public void GetFilesToDelete(string directoryPath)
+        {
+            filesToDelete = new List<string>();
+
+            // Get all files in the specified directory and its subdirectories
+            foreach (string filePath in Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories))
+            {
+                filesToDelete.Add(filePath.Split(new[] { "tmp\\" }, StringSplitOptions.RemoveEmptyEntries)[1]);
+            }
+        }
+        public async void CleanServerAPI()
+        {
+            string apiFilePath = ServerPath.GetServersServerFiles(_serverData.ServerID, serverAPIFileName);
+            string tmpDestination = ServerPath.GetServersServerFiles(_serverData.ServerID, @"tmp\");
+            string directoryPath = ServerPath.GetServersServerFiles(_serverData.ServerID, @"ShooterGame\Binaries\Win64\");
+
+            // Extract to get File List
+            if (!await FileManagement.ExtractZip(apiFilePath, Directory.GetParent(tmpDestination).FullName))
+            {
+                Error = $"Fail to extract {serverAPIFileName}";
+            }
+
+            // Get List of ServerAPI files
+            GetFilesToDelete(Directory.GetParent(tmpDestination).FullName);
+
+            // Delete each file in the array
+            foreach (string fileName in filesToDelete)
+            {
+                string filePath = Path.Combine(directoryPath, fileName);
+
+                try
+                {
+                    if (File.Exists(filePath))
+                    {
+                        File.Delete(filePath);
+                        Notice = $"File '{fileName}' deleted successfully.";
+                    }
+                    else if (Directory.Exists(filePath))
+                    {
+                        // Delete the directory and its contents recursively
+                        Directory.Delete(filePath, true);
+                        Notice = $"Directory '{filePath}' deleted successfully.";
+                    }
+                    else
+                    {
+                        Notice = $"File '{fileName}' does not exist.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Error = $"Error deleting file '{fileName}': {ex.Message}";
+                }
+            }
+
+            // Delete tmp
+            Directory.Delete(tmpDestination, true);
+        }
+        public async void InstallServerAPI()
+        {
+            try
+            {
+                string apiFilePath = ServerPath.GetServersServerFiles(_serverData.ServerID, serverAPIFileName);
+                string apiDestination = ServerPath.GetServersServerFiles(_serverData.ServerID, @"ShooterGame\Binaries\Win64\");
+
+                // Install
+                if (!await FileManagement.ExtractZip(apiFilePath, Directory.GetParent(apiDestination).FullName))
+                {
+                    Error = $"Fail to extract {serverAPIFileName}";
+                }
+
+                // Delete zip file
+                await FileManagement.DeleteAsync(apiFilePath);
+
+                Notice = "Extraction completed successfully.";
+            }
+            catch (Exception ex)
+            {
+                Error = $"Error extracting package: {ex.Message}";
+            }
+        }
+        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Notice += $"Download Progress: {e.ProgressPercentage}";
+        }
+
+        private void WebClient_DownloadFileCompletedVCPackage(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                Error = $"Download failed: {e.Error.Message}";
+            }
+            else
+            {
+                Notice = "Download completed successfully. Installing requirements now...";
+                InstallVCPackage();
+            }
+        }
+
+        private void WebClient_DownloadFileCompletedServerAPI(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                Error = $"Download failed: {e.Error.Message}";
+            }
+            else
+            {
+                Notice = "Download completed successfully. Extracting server API now...";
+                InstallServerAPI();
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # WindowsGSM.ArkSAwithServerAPI
 🧩WindowsGSM plugin that provides Ark Survival Ascended Dedicated server with ServerAPI support from GameServerHUB
+
+
+# WindowsGSM Installation: 
+1. Download  WindowsGSM https://windowsgsm.com/ 
+2. Create a Folder at a Location you wan't all Server to be Installed and Run.
+4. Drag WindowsGSM.Exe into previoulsy created folder and execute it.
+
+# Plugin Installation:
+1. Download [latest](https://github.com/ohmcodes/WindowsGSM.ArkSAwithServerAPI/releases/latest) release
+2. Extract then Move **ArkSAwithServerAPI.cs** folder to **plugins** folder
+3. OR Press on the Puzzle Icon in the left bottom side and install this plugin by navigating to it and select the Zip File.
+4. Click **[RELOAD PLUGINS]** button or restart WindowsGSM
+5. Navigate "Servers" and Click "Install Game Server" and find "American Truck Simulator Dedicated Server [ArkSAwithServerAPI.cs]
+
+### Official Documentation (old but still the same)
+🗃️ https://ark.fandom.com/wiki/Dedicated_server_setup
+
+### Unofficial Documentation
+🗃️ https://www.liquidweb.com/blog/ark-dedicated-server/
+
+### The Game
+🕹️ https://store.steampowered.com/app/2399830/ARK_Survival_Ascended/
+
+### Dedicated server info
+🖥️ https://steamdb.info/app/2430930/info/
+
+### ServerAPI Link
+🖥️ https://gameservershub.com/forums/resources/ark-survival-ascended-serverapi-crossplay-supported.683/
+
+# Features
+- Automatic update ServerAPI and Permission plugin
+- Able to update The ServerAPI Manually via Update button
+- Can Stop/Start/Restart/Validate
+- Auto Download and Install required VC Package
+
+# License
+This project is licensed under the MIT License - see the <a href="https://github.com/ohmcodes/WindowsGSM.ArkSAwithServerAPI/blob/main/LICENSE">LICENSE.md</a> file for details
